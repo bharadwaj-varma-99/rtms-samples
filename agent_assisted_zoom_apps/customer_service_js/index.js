1d1dc1f9397fc67b8d384b04a380f97374d02f2e
tanchunsiong <tanchunsiong@outlook.sg>
2025-07-04 16:47:55 +0800

    fixed corrupted video with more code checks

--- agent_assisted_zoom_apps/customer_service_js/index.js
+++ agent_assisted_zoom_apps/customer_service_js/index.js
@@ -12,6 +12,7 @@ import { fileURLToPath } from 'url';
 import { preloadRetrieverOnce, askLLMWithTranscript } from './ragPipeline.js';
 
  await preloadRetrieverOnce();
+ 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 let frameCounter = 0;
